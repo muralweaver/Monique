66ed6b6acec58dbdb76b12abb53fda379bba80c8
Awi Mphaphuli <awi@benchmark.digital>
2020-03-25 12:31:23 +0200

    project rename

--- monique/monique/urls.py
+++ monique/monique/urls.py
@@ -1,4 +1,4 @@
-"""bubbie URL Configuration
+"""monique URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
