0a87333c9f4a2d3537b9f2d8df8ab22e6c6791d5
muralweaver <almphaphuli@tuta.io>
2020-04-01 13:29:03 +0200

    Begin REST API build with Djangorestframework

--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Contact
+from .serializers import ContactSerializer 
+from rest_framework import generics
+
 
 def home(request):
     return HttpResponse('Hello World')
+
+class ContactListCreate(generics.ListCreateAPIView): 
+    queryset = Contact.objects.all() 
+    serializer_class = ContactSerializer
